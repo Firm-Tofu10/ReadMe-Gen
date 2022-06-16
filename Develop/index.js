f2f97e44f165a93be4f82ce8fd75bf81625d37cd
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-06-15 17:14:19 -0700

    15 minute mark

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message:"title",
+    }, 
+        {
+            type:"input",
+            name:"description",
+            message:"enter description",
+        },
+        {
+            type:"input",
+            name:"instalation",
+            message:"enter instalation instructions",
+        },
+        {
+            type:"input",
+            name:"testing",
+            message:"enter testing instructions",
+        },
+        {
+            type:"input",
+            name:"usage",
+            message:"enter usage instructions",
+        },
+        {
+            type:"input",
+            name:"contributers",
+            message:"enter contributers instructions",
+        },
+        {
+            type:"list",
+            name:"license",
+            message:"choose license",
+            choices:["MIT","ISC","GPL","APACHE2.0","NONE"]
+        },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
